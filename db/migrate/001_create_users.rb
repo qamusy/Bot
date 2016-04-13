786e9d2e1c3fc367bbb5eab87b917c5e1bb650a1
MhdAli <ma.alfarra@gmail.com>
2016-04-13 08:58:03 +0800

    Added create_date and location for users table

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -7,6 +7,8 @@ class CreateUsers < ActiveRecord::Migration
       t.text :last_name
       t.text :lang, :default => "en"
       t.text :last_command
+      t.date :create_date
+      t.text :location
     end
   end
 end
